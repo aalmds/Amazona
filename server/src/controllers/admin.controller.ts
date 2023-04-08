17779b7354baaaa2a5ed4e8acd0c5a9600296f77
Raafm <raafm@cin.ufpe.br>
2023-04-08 15:21:19 -0300

    feat(adm): add function to get monthly stats

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -5,11 +5,26 @@ import { BaseRepository } from '../repositories/base.repository';
 import OrderModel from '../models/order.model';
 import OrderRepository from '../repositories/order.repository';
 
-let distribuicao_estatistica: ((vals : number[], granularity : number) => number[]) = function (
+let getMesIndexFromOrder: (ordem: OrderEntity) => Number = function(
+    ordem : OrderEntity
+    ): Number {
+        let mesString =  ordem.purchaseDate;
+        let mesIndex = mesString.split("-")[1]; //string do mes, ex: '02' == fevereiro
+        return Number(mesIndex)-1; //subtrai 1 para deixar 0 indexado
+}   
+
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let distribuicaoEstatistica: ((vals : number[], granularity : number) => number[]) = function (
     vals: number[],
     granularity : number
 ): number[] {
-    
+
+
     var sorted_vals: number[] = vals.sort((n1,n2) => n1 - n2);
     
     let minimo = sorted_vals[0]; //Math.min(...vals);
@@ -22,7 +37,7 @@ let distribuicao_estatistica: ((vals : number[], granularity : number) => number
     var bin_index = 0;
 
     let j = 0;
-    while (j <= sorted_vals.length) {
+    while (j < sorted_vals.length) {
         let valor = sorted_vals[j];
         if ( (cur_bin <= valor) && (sorted_vals[j] <  next_bin) )  {
             dist[bin_index]++;
@@ -58,14 +73,15 @@ export default class AdminController {
         this.db = new OrderRepository()
     }
 
+    // funca que gera ordens aleatorias a partir de uma ordem base ja feita 
     private async generateOrders(numberOfOrders : number) {
-        const baseOrder = (await this.db.getOrder()).at(0);
-        baseOrder?.purchaseDate
+        const baseOrder = (await this.db.getOrder()).at(0); //pega uma ordem como base
+        //baseOrder?.purchaseDate
         for (let i :number = 0; i < numberOfOrders; i++){
             await this.db.createOrder(new OrderEntity({
                 userId:         baseOrder?.id + String(i),
                 totalValue:     String((Number(baseOrder?.totalValue)*Math.random())) ,
-                purchaseDate:   baseOrder?.purchaseDate    ,
+                purchaseDate:   "2023-" + String(getRandomInt(1,13)) + "-29T06:00:00Z"   ,
                 statusHistory:  baseOrder?.statusHistory   ,
                 productsIds:    baseOrder?.productsIds     , 
                 address :       baseOrder?.address         ,
@@ -77,31 +93,44 @@ export default class AdminController {
         this.router.get(this.prefix, async (req , res ) => { 
             
             let orders_arr : OrderEntity[] = (await this.db.getOrder()) // pegar todos
-            let recentOrders = orders_arr.filter((order) => order.purchaseDate > "20/10") //filtra
-            let totalValues = orders_arr.map((order) => Number(order.totalValue)) //aplica operacao em todos
-            let totalValues_filter_data = orders_arr.map((order) => { // retorna undifined em alguns casos
-               if(order.purchaseDate > "20/02")
-                    return order.totalValue
-            })
+            let currentMonth = new Date().getMonth()
+            
+            //filtra somente orden do mes atual
+            let monthlyOrders = orders_arr.filter((order) => getMesIndexFromOrder(order) == currentMonth);
+            
+            let monthlyValues = monthlyOrders.map((order) => Number(order.totalValue));
+            let totalValues = orders_arr.map((order) => Number(order.totalValue)); //aplica operacao em todos
+            // let totalValues_filter_data = orders_arr.map((order) => { // retorna undifined em alguns casos
+            //    if(order.purchaseDate > "20/02")
+            //         return order.totalValue
+            // })
         
             
-            console.log(orders_arr);
             res.status(200);
             res.send({
-                "all": totalValues,
-                "max": Math.max(...totalValues),
-                "min": Math.min(...totalValues),
-                "mean": totalValues.reduce((a, b) => a + b, 0) / totalValues.length,
-                //"distribution": distribuicao_estatistica(totalValues_filter_data, 10),
-                
+                "Ever":{
+                            "all": totalValues,
+                            "max": Math.max(...totalValues),
+                            "min": Math.min(...totalValues),
+                            "mean": totalValues.reduce((a, b) => a + b, 0) / totalValues.length,
+                            "distribution": distribuicaoEstatistica(totalValues, 5),
+                        },
+                "Monthly":{
+                            "all": monthlyValues,
+                            "max": Math.max(...monthlyValues),
+                            "min": Math.min(...monthlyValues),
+                            "mean": monthlyValues.reduce((a, b) => a + b, 0) / monthlyValues.length,
+                           "distribution": distribuicaoEstatistica(monthlyValues, 5),
+                        }
+                });
                 });
             
 
             //this.db.add()
-        })
     }
-
 }
+
+
 
 // C:\Users\rodri\Documents\GitHub\Amazona\server\src\data\orders.json
 // C:\Users\rodri\Documents\GitHub\Amazona\server\dist\src\data\orders.json
