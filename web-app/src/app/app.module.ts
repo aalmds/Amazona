1afdf7a9b1577d9279064f5517ed680cd5a5e837
Victor Silva <victormarques.ia@gmail.com>
2023-04-12 00:22:49 -0300

    fix(*): remove duplicated imports on app.module

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,7 +9,6 @@ import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { OrderModule } from './order/order.module';
 import { SharedModule } from './shared/shared.module';
 
